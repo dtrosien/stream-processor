9d0f0d08a24e6acbd8ebdb9b892639a64e9d3a55
dtrosien <dtrosien@gmail.com>
2024-08-01 18:54:42 +0200

    first draft of blocking impl

--- src/container/mod.rs
+++ src/container/mod.rs
@@ -1,15 +1,18 @@
 use crate::type_definitions::{MixedBatch, UniformBatch};
 use arrow::array::RecordBatch;
 use std::any::Any;
+use std::collections::HashMap;
 use std::sync::Arc;
 
 pub trait BatchContainer {
     fn get_sink(self: Arc<Self>) -> Option<String>;
     fn get_batch(self: Arc<Self>) -> Arc<Batch>;
     fn get_batch_name(self: Arc<Self>) -> Option<String>;
+
+    fn get_meta(self: Arc<Self>) -> Option<HashMap<String, String>>;
 }
 
-// todo maybe include batch infos for commit
+// todo maybe include batch infos for commit (meta_hashmap can be used for example)
 
 pub struct GenericBatchContainer {
     batch: Arc<Batch>,
@@ -38,5 +41,9 @@ impl BatchContainer for GenericBatchContainer {
 
     fn get_batch_name(self: Arc<Self>) -> Option<String> {
         self.batch_name.clone()
+    }
+
+    fn get_meta(self: Arc<Self>) -> Option<HashMap<String, String>> {
+        None
     }
 }
